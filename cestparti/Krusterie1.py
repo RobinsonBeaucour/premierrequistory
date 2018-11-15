1eaaa02ffa5654f4f0578e2088c140f4c3e0f38c
RobinsonBeaucour <robinson.beaucour@student-cs.fr>
2018-11-15 16:22:03 +0100

    premieres modifs

---
+++ cestparti/Krusterie1.py
@@ -0,0 +1,12 @@
+def krust():
+    while True:
+        a=input('C\'est un réseau?')
+        if a =='oui':
+            print('\n Mais c\'est un réseau!!!! \n')
+            break
+        if a=='non':
+            print('\n Mais c\'est un réseau!!!! \n')
+            break
+        else:
+            print('\n \"des\" ça veut dire \"de les\"!!!! \n')
+krust()
---
+++ cestparti/Krusterie2.py
@@ -0,0 +1,8 @@
+def krust_bis():
+    while True:
+        s=input('hum?')
+        if s=='Non NonNon NonNon':
+            break
+        else:
+            print('Non NonNon NonNon')
+krust_bis()
